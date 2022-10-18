e4e60d9623932039805f6450fc69a2def71bba8e
Mark Lippert <mark@foresitegrp.com>
2022-10-18 09:44:55 -0500

    Remove Wordpress update emails

--- wp-content/themes/wpf/functions.php
+++ wp-content/themes/wpf/functions.php
@@ -1,7 +1,12 @@
 <?php
-// I don't need to be notified about every automatic update
-add_filter( 'auto_plugin_update_send_email', '__return_false' );
-add_filter( 'auto_theme_update_send_email', '__return_false' );
+// Don't send emails when Wordpress updates happen
+add_filter('auto_core_update_send_email', 'stop_auto_update_emails', 10, 4);
+function stop_update_emails($send, $type, $core_update, $result) {
+  if (!empty($type) && $type == 'success') return false;
+  return true;
+}
+add_filter('auto_plugin_update_send_email', '__return_false');
+add_filter('auto_theme_update_send_email', '__return_false');
 
 
 // We want Featured Images on Pages and Posts
