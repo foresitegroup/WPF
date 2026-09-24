afce2b523b16a0a1402512afba48300d51ffbb3a
Mark Lippert <mark@foresitegrp.com>
2026-09-24 09:52:28 -0500

    Address changes

--- wp-content/themes/wpf/template-contact-page.php
+++ wp-content/themes/wpf/template-contact-page.php
@@ -60,15 +60,15 @@ endif;
     <div id="maps-content">
       <div>
         <div class="map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2916.171091534933!2d-87.9220324845221!3d43.037831579147124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8805199dbf77da43%3A0x7f96bc00c3569c4b!2s633+W+Wisconsin+Ave%2C+Milwaukee%2C+WI+53203!5e0!3m2!1sen!2sus!4v1526051984757" width="600" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3459.7463286391467!2d-87.92174292349489!3d43.027358392682736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8805199129dc9fb5%3A0xcd0bd5232b277f50!2s600%20W%20Virginia%20St%2C%20Milwaukee%2C%20WI%2053204!5e1!3m2!1sen!2sus!4v1785427058755!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
         </div>
       </div>
 
       <div>
         <h3>Milwaukee</h3>
         Wisconsin Policy Forum<br>
-        633 W. Wisconsin Ave. Suite #406<br>
-        Milwaukee, WI 53203<br>
+        600 W. Virginia St. Suite 502<br>
+        Milwaukee, WI 53204<br>
         <br>
 
         <span>Phone:</span> 414.276.8240
@@ -77,8 +77,8 @@ endif;
       <div>
         <h3>Madison</h3>
         Wisconsin Policy Forum<br>
-        PO Box 259403<br>
-        Madison, WI 53725<br>
+        30 W. Mifflin St. Suite 806<br>
+        Madison, WI 53703<br>
         <br>
 
         <span>Phone:</span> 608.241.9789
